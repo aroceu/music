d3854491aa89d0221bc496f3c3ffec8def37b58b
arrow <dev@aroceu.cloud>
2024-10-28 14:01:19 -0400

    Create 2018.php

---
+++ yearly/lists/2018.php
@@ -0,0 +1,391 @@
+<table class="song">
+				<tr><td id="title">Birthday</td><td></td>
+				<td class="artist">All Time Low</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">My First Kiss</td><td></td>
+				<td class="artist">3OH!3 ft. Kesha (<i>Streets of Gold</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Let Me</td><td></td>
+				<td class="artist">ZAYN</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Somber</td><td></td>
+				<td class="artist">Violet Days ft. morgxn</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Sway</td><td></td>
+				<td class="artist">Tove Styrke (<i>Sway</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Sober Up</td><td></td>
+				<td class="artist">AJR ft. Rivers Cuomo</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">I Wanna Get Better</td><td></td>
+				<td class="artist">ATC & The Ready Set</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Bullseye</td><td></td>
+				<td class="artist">Aly & AJ (<i>Imnsomniatic</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Potential Breakup Song</td><td></td>
+				<td class="artist">Aly & AJ (<i>Imnsomniatic</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Closure</td><td></td>
+				<td class="artist">Aly & AJ (<i>Imnsomniatic</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Division</td><td></td>
+				<td class="artist">Aly & AJ (<i>Imnsomniatic</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Potential Breakup Song</td><td></td>
+				<td class="artist">Aly & AJ (<i>Imnsomniatic</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Like It or Leave It</td><td></td>
+				<td class="artist">Aly & AJ (<i>Imnsomniatic</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Like Whoa</td><td></td>
+				<td class="artist">Aly & AJ (<i>Imnsomniatic</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Chemicals React (remix)</td><td></td>
+				<td class="artist">Aly & AJ (<i>Imnsomniatic</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Don't Go Breaking My Heart</td><td></td>
+				<td class="artist">Backstreet Boys</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Ignore Me</td><td></td>
+				<td class="artist">Betty Who</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Look Back</td><td></td>
+				<td class="artist">Betty Who</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Taste</td><td></td>
+				<td class="artist">Betty Who</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">who you are</td><td></td>
+				<td class="artist">blackbear</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">DDU-DU DDU-DU</td><td></td>
+				<td class="artist">BLACKPINK (<i>SQUARE UP</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Ready or Not</td><td></td>
+				<td class="artist">Bridgit Mendler (<i>Hello My Name Is...</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Hurricane</td><td></td>
+				<td class="artist">Bridgit Mendler (<i>Hello My Name Is...</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Finesse (remix)</td><td></td>
+				<td class="artist">Bruno Mars ft. Cardi B</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Never Be the Same</td><td></td>
+				<td class="artist">Camila Cabello (<i>Camila</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">I Like It</td><td></td>
+				<td class="artist">Cardi B ft. Bad Bunny & J. Balvin</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">APESHIT</td><td></td>
+				<td class="artist">The Carters</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Take You Home</td><td></td>
+				<td class="artist">Cassadee Pope</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Only You</td><td></td>
+				<td class="artist">Cheat Codes ft. Little Mix</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">This Is America</td><td></td>
+				<td class="artist">Childish Gambino</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Bury It</td><td></td>
+				<td class="artist">CHVRCHES ft. Hayley Williams</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Get Out</td><td></td>
+				<td class="artist">CHVRCHES (<i>Love Is Dead</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Growing Pains</td><td></td>
+				<td class="artist">COIN</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Wisdom Teeth</td><td></td>
+				<td class="artist">cupcakKe (<i>Ephorize</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Crayons</td><td></td>
+				<td class="artist">cupcakKe (<i>Ephorize</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Sit Still, Look Pretty</td><td></td>
+				<td class="artist">Daya</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Sober</td><td></td>
+				<td class="artist">Demi Lovato</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Hollow</td><td></td>
+				<td class="artist">DNCE ft. Sekai No Owari</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Lake Effect Kid</td><td></td>
+				<td class="artist">Fall Out Boy</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Sit Next to Me</td><td></td>
+				<td class="artist">Foster The People</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">What I Need</td><td></td>
+				<td class="artist">Hayley Kiyoko ft. Kehlani (<i>Expectations</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Under the Blue / Take Me In</td><td></td>
+				<td class="artist">Hayley Kiyoko (<i>Expectations</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Curious</td><td></td>
+				<td class="artist">Hayley Kiyoko (<i>Expectations</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Let It Be</td><td></td>
+				<td class="artist">Hayley Kiyoko (<i>Expectations</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Crazy, Classic, Life</td><td></td>
+				<td class="artist">Janelle Monae (<i>Dirty Computer</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Screwed</td><td></td>
+				<td class="artist">Janelle Monae ft. Zoe Kravitz (<i>Dirty Computer</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Django Jane</td><td></td>
+				<td class="artist">Janelle Monae (<i>Dirty Computer</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Pynk</td><td></td>
+				<td class="artist">Janelle Monae ft. Grimes (<i>Dirty Computer</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Make Me Feel</td><td></td>
+				<td class="artist">Janelle Monae (<i>Dirty Computer</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">You and I Both</td><td></td>
+				<td class="artist">Jason Mraz</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Undercover</td><td></td>
+				<td class="artist">Kehlani (<i>SweetSexySavage</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Kiss The Boy</td><td></td>
+				<td class="artist">Keiynan Lonsdale</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Preach</td><td></td>
+				<td class="artist">Keiynan Lonsdale</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Good Life</td><td></td>
+				<td class="artist">Keiynan Lonsdale</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Love Lies</td><td></td>
+				<td class="artist">Khalid ft. Normani</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Heart to Break</td><td></td>
+				<td class="artist">Kim Petras</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">I Heart U</td><td></td>
+				<td class="artist">Kretsen ft. Violet Days</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Chasing Fire</td><td></td>
+				<td class="artist">Lauv</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Getting Over You</td><td></td>
+				<td class="artist">Lauv (<i>I met you when I was 18.</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">I Like Me Better</td><td></td>
+				<td class="artist">Lauv (<i>I met you when I was 18.</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Bracelet</td><td></td>
+				<td class="artist">Lauv (<i>I met you when I was 18.</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Astoria</td><td></td>
+				<td class="artist">Marianas Trench (<i>Astoria</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Burning Up</td><td></td>
+				<td class="artist">Marianas Trench (<i>Astoria</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Yesterday</td><td></td>
+				<td class="artist">Marianas Trench (<i>Astoria</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">This Means War</td><td></td>
+				<td class="artist">Marianas Trench (<i>Astoria</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Rhythm of Your Heart</td><td></td>
+				<td class="artist">Marianas Trench (<i>Astoria</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Hands off My Heart / Places You Go</td><td></td>
+				<td class="artist">MKTO</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">American Dream</td><td></td>
+				<td class="artist">MKTO (<i>MKTO</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Superstitious</td><td></td>
+				<td class="artist">MKTO</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Colour</td><td></td>
+				<td class="artist">MNEK ft. Hailee Steinfeld</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Tongue</td><td></td>
+				<td class="artist">MNEK</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Beach Bones</td><td></td>
+				<td class="artist">More Amor ft. Ryan Ross</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Let's Fall In Love</td><td></td>
+				<td class="artist">Mother Mother (<i>The Sticks</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Business Man</td><td></td>
+				<td class="artist">Mother Mother (<i>The Sticks</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Chun-Li</td><td></td>
+				<td class="artist">Nicki Minaj</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Nasty Majesty</td><td></td>
+				<td class="artist">Off the Hook (<i>Splatoon 2 OST</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">Color Pulse</td><td></td>
+				<td class="artist">Off the Hook (<i>Splatoon 2 OST</i>)</td></tr>
+			</table>
+
+<table class="song">
+				<tr><td id="title">(Fuck A) Silver Lining</td><td></td>
+				<td class="artist">Panic! at the Disco (<I>Pray for the Wicked</I>)</td></tr>
+			</table>
+
+<!-- not done, resume at home -->
