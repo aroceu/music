11aa2c5f847b1f1f6a4ea623e5f568e78ea33eb6
Arrow Sparrow <dev@aroceu.cloud>
2024-10-28 22:53:47 -0400

    Added files from cpanel

---
+++ index.php
@@ -0,0 +1,99 @@
+<html lang="en">
+    
+    <head>
+
+    	<meta name="viewport" content="initial-scale=1.0">
+
+    	<link rel="shortcut icon" type="image/x-icon" href="hk.png" />
+            <meta name="robots" content="noai, noimageai">
+
+            <link rel="preconnect" href="https://fonts.googleapis.com">
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+            <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200..700&family=Rochester&display=swap" rel="stylesheet">
+
+
+        <title>aroceu's music log</title>
+        
+        <!-- theme after current year, or previous if current year's colors have not been decided -->
+		<style type="text/css">
+
+
+		:root{
+			--body: #E8A7BD linear-gradient(to top left, #E8A7BD 0%, #9384B5 100%);
+			--body-border: #9384B5;
+			--container: #E8A7BD linear-gradient(to top left, #9384B5 0%, #E8A7BD 100%); 
+			--h1: #9384B5;
+			--h2: #333;
+			border:;
+			--accent: #9384B5;
+		}
+
+		#menu{
+			margin: -2em auto 0;
+			width: 75%;
+		}
+
+		#menu ul{
+			list-style-type: none;
+			text-align: center;
+		}
+
+		#menu ul li{
+			-moz-padding-start: none!important;
+			padding-start: none!important;
+			-webkit-padding-start: none!important;
+		}
+
+		#menu ul li a:link, #menu ul li a:visited{
+			display: block;
+			width: 100%;
+			font: normal 180% 'Inconsolata';
+			text-decoration: none;
+			background: #9384B5;
+			color: #333;
+		}
+
+		#menu ul li a:hover, #menu ul li a:focus{
+			background: #E8A7BD;
+		}
+
+		</style>
+
+		<link rel="stylesheet" href="yearly/style.css" type="text/css" media="screen" />
+
+	</head>
+	
+	<body>
+
+		<div id="container">
+
+		<h1>aroceu's music log</h1>
+		
+		<div id="blurb">
+		
+		<p>Welcome to my music log, a small section of my website that mostly serves as a text-based archive of music I enjoy. Below you'll see my yearly playlists (which is a big jumble of sometimes random genres); and eventually I'll have more genre-based playlists, because this section is technically:</p>
+		
+		<p align="center"><big>[ UNDER CONSTRUCTION ]</big></p>
+		
+		</div>
+		
+	    <div id="menu">
+		
+		<ul>
+		    <li><a href="yearly">Yearly Playlist</a></li>
+		</ul>
+		
+		</div>
+
+		<div id="footer">
+			&copy; 2024 <a href="https://aroceu.com">aroceu</a> &#127926; <a href="/">music</a>
+		</div>
+
+	</div>
+
+	</div>
+
+</body>
+
+</html>
+		
