2574eb3c29b834855df2db154de4d28563347f9c
Arrow Sparrow <dev@aroceu.cloud>
2024-11-21 10:17:10 -0500

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,22 @@
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
             <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200..700&family=Rochester&display=swap" rel="stylesheet">
 
+            <script type="text/javascript" src="https://kingdra.net/x/animatedcollapse.js"></script>
+
+            <script type="text/javascript">
+            	animatedcollapse.addDiv('musician', 'fade=1, speed=400, hide=1');
+            	animatedcollapse.addDiv('genres', 'fade=1, speed=400, hide=1');
+
+				animatedcollapse.ontoggle=function($, divobj, state){ //fires each time a DIV is expanded/contracted
+					//$: Access to jQuery
+					//divobj: DOM reference to DIV being expanded/ collapsed. Use "divobj.id" to get its ID
+					//state: "block" or "none", depending on state
+				};
+
+				animatedcollapse.init();
+
+</script>
+
     	<title>aroceu's music log</title>
 
 
@@ -65,12 +81,33 @@
 
 		<div id="container">
 
-		<h1>aroceu's yearly playlists</h1>
+		<h1>aroceu's music log</h1>
 
 		<div id="blurb">
-			<p>I have a methodology to these, and I've been maintaining them since the oldest one listed on here across my music platforms, from iTunes to Spotify. This also includes the subtitles and the colors, somewhat, which reflect how I felt about the year and how it might affect my music-listening habits.</p> 
+			<p>Music is and has always been the one consistency in my life. <a href="javascript:animatedcollapse.toggle('musician')" class="footnote">Though I'm not much of a musician</a>, I've been a fan of the art of a tune since I could talk.</p>
 
-			<p>Either way, I listen to music the same way I do anything else: by hyperfixating on it regardless of when it came out &mdash; though that can have its impacts &mdash; and then organizing. This is a result of that.</p>
+			<div id="musician">
+
+				<p>I play piano and understand singing though I don't think of myself as an exceptional singer &mdash; I used to be in choir because I enjoyed it, but only before college. I have some experience with clarinet and I want to try guitar again, but that ambition's in the far back of my mind.</p>
+
+			</div>
+
+			<p>I like all genres: boy bands and pop girls have been my thing since the Backstreet Boys's "I Want It That Way" (my first pop song &mdash; I listened to it repeatedly on a casette tape) and <i>Metamorphosis</i> by Hilary Duff. I got into rock, R&B, and rap/hip-hop around 2005; then pop-punk/alternative and k-pop in 2009. I've dipped into <a href="javascript:animatedcollapse.toggle('genres')" class="footnote">many other genres here and there</a> so don't let my penchance for mainstream music be definitive of my taste. Pop is just my favorite genre.</p>
+
+			<div id="genres">
+
+				<p>A non-exhaustive list of additional musical genres I enjoy: indie (pop/rock/other), folk, Thai, ska, country, electronic, musicals/showtunes, video games, classical (the only thing my parents listen to), new wave, comedy, blues, gospel/Christian (I used to be religious and some of those songs still slap), house, trap, swing, someone I dated was really into EDM and dubstep, funk, metal/screamo, mandopop, jrock/jpop... and there's probably more!</p>
+
+				<p>I'm also a huge fan of acapellas, covers, mashups, and acapella cover mashups as well. They're like music "fanfiction" to me &mdash; and you know how much I like fanfiction.</p>
+
+			</div>
+
+			<p>Listening to music is the one thing I refuse to feel pretentious about, so everything I've enjoyed is a conglomeration of both my own enjoyment and influences from the people around me throughout my life. Since 2012, I've been keeping annual playlists of songs I've been obsessed with, or at least had on repeat for some portion of the year. Occasionally I'm an album listener, though it's not integral to my music enjoyment. I have a methodology to compiling these playlists, and have been maintaining them even as I switched music platforms (from iTunes to Spotify) in 2017.</p>
+
+			<p>Though this is technically an archive of music, it's also a peek into my life. These playlists include subtitles and colors to theme the year, which tend to reflect how I felt about the year and might affect my music-listening habits. I'm still navigating my journey along with everyone else &mdash; but I used to have those profile buttons that said "music is my life/drug" on my old MySpace profile, so this is really just a continuation of that. Full circle and whatnot.</p>
+
+			<p>Anyway, I listen to music the same way I do anything else: by hyperfixating on it regardless of when it came out (though that can have its impacts) and then organizing. This is a result of that.</p>
+
 		</div>
 		
 		<div id="menu">
