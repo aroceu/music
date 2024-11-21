5f39b526dbfaa1961a09788d417858a01d09357a
Arrow Sparrow <dev@aroceu.cloud>
2024-11-21 11:25:31 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <div style="clear: both;"></div>
 
 		<div id="footer">
-			&copy; 2024 <a href="https://aroceu.com">aroceu</a> &#127926; <a href="https://aroceu.com/music">music</a> &gt; <?php echo basename($path,".php"); ?>
+			&copy; 2024 <a href="https://aroceu.com">aroceu</a> &#127926; <a href="https://aroceu.com/music">music</a> &gt; <?php  echo basename($_SERVER['PHP_SELF'], ".php"); ?>
 		</div>
 
 	</div>
