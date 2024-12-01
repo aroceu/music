5ff402bbd2c6512b336c60f13f52a462db90cb43
Arrow Sparrow <dev@aroceu.cloud>
2024-12-01 16:22:41 -0500

    Update index.php

--- index.php
+++ index.php
@@ -229,7 +229,9 @@
 
 		</div>
 
-		
+		<div class="mobilenote">
+		This website is best viewed on a larger desktop. Please switch to a screen resolution of 1280 x 1024 or higher for the best experience.
+	</div>
 
 		<div id="footer">
 			&copy; 2024 <a href="https://aroceu.com">aroceu</a> &#127926; music</a>
