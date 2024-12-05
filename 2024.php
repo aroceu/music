73ecb871d6ce5f99101961ee4c747ce3ad309f2b
Arrow Sparrow <dev@aroceu.cloud>
2024-12-04 20:19:37 -0500

    Update 2024.php

--- 2024.php
+++ 2024.php
@@ -26,7 +26,11 @@
 		<h1>arrow's year in music: 2024</h1>
 		<h2>actually i won this year</h2>
 
-		<div id="blurb"><p>TBD</p>
+		<div id="blurb"><p>The music of 2024 was what inspired me to create this subsection of my website in the first place, because every instance of mainstream American music this year has been straight up my alley. From Taylor Swift (she was my #1 streamed Spotify artist. again. because of "imgonnagetyouback" (and Guilty as Sin)) to Kendrick Lamar and my long-awaited return to rap/hip-hop. Plus getting hit full force with the BLACKPINK hyperfixation.</p>
+
+			<p>Even then, I would still consider <i>eternal sunshine</i> and <i>Cowboy Carter</i> my top two albums of this year. Oh, and <i>Warriors</i>. I started listening to <i>The Rise and Fall of a Midwest Princess</i> at the end of last year after a recommendation from my brother (namely, Red Wine Supernova which took me completely by storm) so I suppose that counts too. And I got into Olivia Rodrigo at the end of last year as well, so that leaked over in the beginning too.</p>
+
+			<p>Jesus Christ, I really did win this year.</p>
 
 		<p align="center"><a href="https://open.spotify.com/playlist/4SkN68D2DzzwC5vKuJ3YMc?si=85a84994da844007" class="button">Listen on Spotify</a>
 			<br /><b>Count:</b> -</p>
