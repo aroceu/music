037adac51a8faa7e1ce1d6952eb3ef6d565d3928
aroceu <dev@aroceu.cloud>
2025-05-12 23:32:44 -0400

    Added header

--- index.php
+++ index.php
@@ -2,11 +2,15 @@
     
     <head>
 
-    	<meta name="viewport" content="initial-scale=1.0">
-    	<meta name="description" content="hello i am an obsessive writer, website owner, and fujoshi, and this is my house.'s music diary"/>
-
+	<meta name="robots" content="noai, noimageai">
+	<meta charset="UTF-8">
+	<meta name="description" content="hello i am an obsessive writer, website owner, and fujoshi, and this is my house."/>
+	<meta property="og:title" content="aroceu (music)" />
+	<meta property="og:description" content="hello i am an obsessive writer, website owner, and fujoshi, and this is my house." />
+	<meta property="og:image" content="https://aroceu.com/assets/preview.png" />
+	<meta property="og:url" content="https://aroceu.com/music" />
+	<meta name="viewport" content="initial-scale=1.0, maximum-scale=5, width=device-width">
     	<link rel="shortcut icon" type="image/x-icon" href="music2024.png" />
-            <meta name="robots" content="noai, noimageai">
 
             <link rel="preconnect" href="https://fonts.googleapis.com">
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
